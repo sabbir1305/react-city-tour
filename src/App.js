b8fc80dc181c5cd8ec596f222dfad169258dfd96
Sabbir Ahmed <sabbir.ove@gmail.com>
2020-06-09 22:19:29 +0600

    React_City

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import './App.scss';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import Navbar from './components/Navbar/Navbar.js';
+import TourList from './components/TourList';
+ class App extends Component {
+
+  render() {
+    return (
+      <>
+      <Navbar/>
+      <TourList/>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
+
---
+++ src/components/TourList/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Tour from '../Tour/Tour.js';
+import './tourlist.scss';
+import {tourData } from "../../tourData.js"
+
+export default class TourList   extends Component {
+
+    state ={
+        tours: tourData 
+    }
+
+    removeTour=id=>{
+        const{tours} = this.state;
+
+        const sortedTours= tours.filter(tour=> tour.id !==id);
+        this.setState({
+            tours: sortedTours
+        });
+        console.log(id);
+        
+    }
+    render() {
+        console.log(this.state.tours);
+        const {tours} = this.state;
+
+        
+        return (
+            <section className="tourlist">
+               {
+                   tours.map(tour => (<Tour key={tour.id} tour={tour} removeTour={this.removeTour}>
+
+                   </Tour>))
+               }
+            </section>
+        )
+    }
+}
